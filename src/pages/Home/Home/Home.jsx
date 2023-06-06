2e339fb8db5ad329010308a07f7b41bf5ad4a29e
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-07 02:55:47 +0600

    carousel added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            home xx lol 
+            <Banner></Banner>
         </div>
     );
 };

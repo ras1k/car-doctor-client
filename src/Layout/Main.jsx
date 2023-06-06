02bf00ccb51e94ec28c2df4ae759e5ad1579f3fa
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-07 02:26:15 +0600

    pages  added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Home from '../pages/Home/Home/Home';
+import Footer from '../pages/Shared/Footer/Footer';
+import NavBar from '../pages/Shared/NavBar/NavBar';
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            home xx lol 
+        </div>
+    );
+};
+
+export default Home;
